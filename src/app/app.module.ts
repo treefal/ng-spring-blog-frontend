e58abdeaadcb21067d2882969976722c46f62e45
user <user@WINDOWS-VK4C0HF>
2021-04-11 01:39:16 +0200

    Add ngx-webstorage dependency.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RegisterSuccessComponent } from './auth/register-success/register-succe
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxWebstorageModule } from 'ngx-webstorage';
 
 @NgModule({
   declarations: [
@@ -24,8 +25,10 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    NgxWebstorageModule.forRoot(),
     RouterModule.forRoot([
       { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
       { path: 'register-success', component: RegisterSuccessComponent },
     ]),
     HttpClientModule,
